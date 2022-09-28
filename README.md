5ae9c7fa002e16af69c10c79c5765578bb3b2061
BobEXP <100017604+BobEXP@users.noreply.github.com>
2022-09-28 20:48:14 +0400

    Publish code
    Publish code

--- README.md
+++ README.md
@@ -1,2 +1,59 @@
-# FFList
- A Fast System File Lister
+<h1 align="center">
+    <br>
+    <img src="https://raw.githubusercontent.com/BobEXP/FFList/main/FFList.png" alt="FFList">
+    <br>
+    FFList
+</h1>
+
+<p align="center">
+    A Fast System File Lister written in Python
+</p>
+
+
+## Features v1.0.0.0
+- [x] Completely Asynchronous
+- [x] Gather All System Files
+- [x] Gather Files From Specified Directory
+- [x] Recursive File Collection
+
+## System Requirements
+
+- [x] Windows 7 / 10 / 11
+- [x] Python 3.11
+
+## Usage
+
+```python
+git clone --depth=1 https://github.com/BobEXP/FFList
+```
+
+```python
+cd FFList
+```
+
+```python
+python -m pip install requirements.txt
+```
+
+```python
+python fflist.py --help
+```
+
+### Examples Use
+
+#### Gather All System Files
+
+```python
+python fflist.py -f "*" -full
+```
+
+#### Gather files from specific directory recursive
+
+```python
+python fflist.py -f "C:/Program Files/"
+```
+
+## License
+
+<a href="https://github.com/BobEXP/FFList/LICENSE" title="License">MIT License</a>
+
---
+++ fflist.py
@@ -0,0 +1,94 @@
+from __future__ import print_function
+from os import listdir, walk, system
+from os.path import isfile, join, realpath, dirname
+from argparse import ArgumentParser
+import asyncio
+
+# saving all the gathered filepaths into list
+total_files = []
+
+curpath = dirname(realpath(__file__))
+
+# progress bar
+def pbar(progress, total):
+	percent = 100 * (progress / float(total))
+	bar = '+' * int(percent) + '-' * (100 - int(percent))
+	print(f"\r|{bar}| {percent:.2f}%", end="\r")
+
+
+# save the list into file
+async def save_files() -> None:
+	filepath = curpath + "/files.txt"
+	with open(filepath, 'w') as f:
+		x = 0
+		while x <= len(total_files)-1:
+			f.write(f"{total_files[x]}\n")
+			x += 1
+
+
+# get the files from folder
+def get_files(dir: str) -> []:
+	tempfiles = [f for f in listdir(dir) if isfile(join(dir, f))]
+	temptotal = dir + "/".join([temp_file for temp_file in tempfiles])[1:]
+	return temptotal
+
+
+# replace with slash
+async def replace_slash(rwith: str) -> str:
+	replaced = rwith.replace("\\", "/")
+	return replaced
+
+
+# append total files
+async def total_appender(dirname: str) -> None:
+	total_files.append(get_files(dirname))
+
+
+# init
+async def init(init_dir: str, isDeep: bool) -> None:
+	system('cls')
+	print(f"Gathering FileSystem Directories from {init_dir}..")
+	dirs_raw = []
+	match isDeep:
+		case True:
+			filenum = 0
+			for x in walk(init_dir):
+				dirs_raw.append(x[0])
+				filenum	+= 1
+				if filenum % 100 == 0:
+					print(f"Folders Gathered: {filenum}")
+		case False:
+			dirs_raw = [x[0] for x in walk(init_dir)]
+	
+	print(f"Gathering files in {len(dirs_raw)} directories..")
+	pbar(0, len(dirs_raw))
+	n = 0
+	while n <= len(dirs_raw)-1:
+		await replace_slash(dirs_raw[n])
+		await total_appender(dirs_raw[n])
+		n += 1
+		pbar(n, len(dirs_raw))
+	print(f"Saving file list...")
+	await save_files()
+
+
+# read config file
+async def main() -> None:
+	parser = ArgumentParser(description="FFlist Argument Parser", add_help=True)
+	parser.add_argument('-f','--folder', type=str, help="Use '*' for --full & 'FOLDER_PATH' for --folder")
+	parser.add_argument('-full','--full', action='store_true', help="All System Files")
+	args = parser.parse_args()
+
+	if args.full:
+		asyncio.gather(init("C:/", True))
+	
+	if args.folder != "*":
+		if args.folder != None:
+			folder = await replace_slash(args.folder)
+			asyncio.gather(init(folder, False))
+		else:
+			print("Please read --help")
+
+
+if __name__ == "__main__":
+	asyncio.run(main())
